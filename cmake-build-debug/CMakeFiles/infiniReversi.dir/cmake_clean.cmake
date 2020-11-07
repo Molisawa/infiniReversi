b4e6eaa488303afb8b0872026b85f14d37c647e7
VAZQUEZ CRUZ <is725664@iteso.mx>
2020-11-06 18:03:43 -0600

    set project

---
+++ cmake-build-debug/CMakeFiles/infiniReversi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/infiniReversi.dir/main.c.obj"
+  "infiniReversi.exe"
+  "infiniReversi.exe.manifest"
+  "infiniReversi.pdb"
+  "libinfiniReversi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/infiniReversi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
